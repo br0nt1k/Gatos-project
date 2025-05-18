c5abb107646fa5abd2807a0f0a2f84b0ffab55fa
KAPAT <sashastorozhuk2@gmail.com>
2025-05-19 00:12:24 +0300

    js_code_for_the_menu_nav

--- src/js/menu.js
+++ src/js/menu.js
@@ -36,3 +36,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+document.querySelectorAll('.header-link, .header-link-now, .modal-item-link, .modal-link').forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Запобігає стандартній навігації
+        document.querySelector('.modal-menu').classList.remove('is-open'); // Закриває модалку
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' }); // Плавний скролінг
+    });
+});
+document.addEventListener("click", event => {
+    const link = event.target.closest(".header-item-link, .header-link, .header-link-now");
+    if (!link) return;
+
+    event.preventDefault();
+    document.querySelector(link.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+});
+
